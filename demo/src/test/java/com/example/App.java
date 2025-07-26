fa3e0eca4c74222dfcfcd52d6acecca536addbad
kavanaghtask1980-max <kavanaghtask1980@gmail.com>
2025-07-26 22:18:50 +0100

    new4

--- demo/src/test/java/com/example/App.java
+++ demo/src/test/java/com/example/App.java
@@ -10,43 +10,36 @@ import com.TestBase.Base;
 
 public class App extends Base {
 
+	String username = "kavanaghtom1980";
+	String password = "Desire97!";
+
     @Test
-    public void testApp2() {
-        getDriver("Chrome");
-		    driver.get("http://dailymail.co.uk");
-    				driver.findElement(By.xpath("//button[@class=\"button_cVc76 primary_vX6Hs full-width_Orq_e consent_BHP9G\"]")).click();
-			String text = driver.findElement(By.xpath("//div[@class=\"links\"]/a[1]")).getText();
-		System.out.println(text);
-		driver.quit();
-	
-    }
+    public void loginPaddyPower() {
+        getDriver("Edge");
+		    driver.get("http://paddypower.co.uk");
+    				driver.findElement(By.xpath("//button[@id=\"onetrust-accept-btn-handler\"]")).click();
+					driver.findElement(By.xpath("//div[@class=\"abc-button__content\"]")).click();
+					driver.findElement(By.xpath("//input[@name=\"username\"]")).click();
+					driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys(username);
+					driver.findElement(By.xpath("//input[@name=\"password\"]")).click();
+					driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys(password);
+					driver.findElement(By.xpath("//button[@name=\"login\"]")).click();
+		 }
 
     @Test
     public void testApp() {
         getDriver("Edge");
-        driver.get("http://google.com");
-        //driver.findElement(By.name("q")).sendKeys("hello");
-        //driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-        driver.quit();
+           driver.get("http://dailymail.co.uk");
+    				driver.findElement(By.xpath("//button[@class=\"button_cVc76 primary_vX6Hs full-width_Orq_e consent_BHP9G\"]")).click();
+					String headline = driver.findElement(By.xpath("//div[@class=\"article article-tri-headline\"]/h2/a")).getText();
+					driver.findElement(By.xpath("//div[@class=\"article article-tri-headline\"]/h2/a")).click();
+
+					
+		System.out.println(headline);
+		driver.quit();
+        
     }
 
-    @Test
-    public void testApp1() {
-        getDriver("chrome");
-        driver.get("https://www.dice.com/jobs");
-
-        int p = 1;
-        while (p != 6) {
-
-            List<WebElement> names = driver.findElements(By.cssSelector("a.card-title-link.bold"));
-            for (int i = 0; i < names.size(); i++) {
-                System.out.println(names.get(i).getText());
-            }
-            System.out.println("--------------------------------------------------------------------------------");
-            driver.findElement(By.xpath("//a[text()='" + (p + 1) + "']")).click();
-            p++;
-        }
+   
 
     }
-
-}
