cf2820058102c298c86a0ed00147ca088d219e0c
kavanaghtask1980-max <kavanaghtask1980@gmail.com>
2025-07-27 19:16:39 +0100

    jenkins test

--- demo/src/test/java/com/example/App.java
+++ demo/src/test/java/com/example/App.java
@@ -1,10 +1,6 @@
 package com.example;
 
-import java.util.List;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -30,10 +26,10 @@ public class App extends Base {
         }
         Assert.assertEquals(title, "Online Betting & Odds | Bet with Paddy Power Sports");
 
-        if (title == "Online Betting & Odds |  with Paddy Power Sports") {
-            System.out.print("nText Match");
+        if (title == "Online Betting & Odds | with Paddy Power Sports") {
+            System.out.print(" Text Match");
         } else {
-            System.out.print("nText does Match");
+            System.out.print(" Text does not not Match");
         }
 
         System.out.print(title);
@@ -51,62 +47,62 @@ public class App extends Base {
 
     }
 
-    @Test
-    public void testApp() {
-        getDriver("Edge");
-        driver.get("http://cnn.com");
-        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
-        System.out.println("Total links " + allLinks.size());
-        //By.xpath("//a")
+    // @Test
+    // public void testApp() {
+    //     getDriver("Edge");
+    //     driver.get("http://cnn.com");
+    //     List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+    //     System.out.println("Total links " + allLinks.size());
+    //     //By.xpath("//a")
 
-        WebElement fifthLink = allLinks.get(5);// webelement - 6th link
-        System.out.println(fifthLink.getText());
-        System.out.println(fifthLink.getAttribute("href"));
-        //fifthLink.click();
-        System.out.println(fifthLink.getLocation().x);
-        System.out.println(fifthLink.getLocation().y);
+    //     WebElement fifthLink = allLinks.get(5);// webelement - 6th link
+    //     System.out.println(fifthLink.getText());
+    //     System.out.println(fifthLink.getAttribute("href"));
+    //     //fifthLink.click();
+    //     System.out.println(fifthLink.getLocation().x);
+    //     System.out.println(fifthLink.getLocation().y);
 
-        for (int i = 0; i < allLinks.size(); i++) {
-            WebElement link = allLinks.get(i);
-            System.out.println(link.getText() + " --- " + link.isDisplayed());
-        }
+    //     for (int i = 0; i < allLinks.size(); i++) {
+    //         WebElement link = allLinks.get(i);
+    //         System.out.println(link.getText() + " --- " + link.isDisplayed());
+    //     }
 
-        //a[@class='_2mylT6']
-        // a._2mylT6
-    }
+    //     //a[@class='_2mylT6']
+    //     // a._2mylT6
+    // }
 
-    @Test
-    public void javaScript() {
-        getDriver("Edge");
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.location='" + paddypower + "'");
-        js.executeScript("document.getElementByID('username').click");
+    // @Test
+    // public void javaScript() {
+    //     getDriver("Edge");
+    //     JavascriptExecutor js = (JavascriptExecutor) driver;
+    //     js.executeScript("window.location='" + paddypower + "'");
+    //     js.executeScript("document.getElementByID('username').click");
 
-        driver.quit();
+    //     driver.quit();
 
-    }
+    // }
 
-    @Test
-    public void printNames() {
-        driver.get("https://www.flipkart.com/mens-clothing/tshirts/pr?sid=2oq,s9b,j9y&otracker=nmenu_sub_Men_0_T-Shirts");
+    // @Test
+    // public void printNames() {
+    //     driver.get("https://www.flipkart.com/mens-clothing/tshirts/pr?sid=2oq,s9b,j9y&otracker=nmenu_sub_Men_0_T-Shirts");
 
-        //List<WebElement> allTshirtNames = driver.findElements(By.xpath("//a[@class='_2mylT6']"));
-        List<WebElement> allTshirtNames = driver.findElements(By.cssSelector("a._2mylT6"));
-        System.out.println("total t shirts - " + allTshirtNames.size());
+    //     //List<WebElement> allTshirtNames = driver.findElements(By.xpath("//a[@class='_2mylT6']"));
+    //     List<WebElement> allTshirtNames = driver.findElements(By.cssSelector("a._2mylT6"));
+    //     System.out.println("total t shirts - " + allTshirtNames.size());
 
-        for (WebElement e : allTshirtNames) {
-            System.out.println(e.getText());
-        }
+    //     for (WebElement e : allTshirtNames) {
+    //         System.out.println(e.getText());
+    //     }
 
-        List<WebElement> allTshirtPrices = driver.findElements(By.xpath("//div[@class='_1vC4OE']"));
-        System.out.println("Total Prices " + allTshirtPrices.size());
-        for (WebElement e : allTshirtPrices) {
-            System.out.println(e.getText());
-        }
-        System.out.println("------------------");
-        for (int i = 0; i < allTshirtNames.size(); i++) {
-            System.out.println(allTshirtNames.get(i).getText() + " --- " + allTshirtPrices.get(i).getText());
-        }
-    }
+    //     List<WebElement> allTshirtPrices = driver.findElements(By.xpath("//div[@class='_1vC4OE']"));
+    //     System.out.println("Total Prices " + allTshirtPrices.size());
+    //     for (WebElement e : allTshirtPrices) {
+    //         System.out.println(e.getText());
+    //     }
+    //     System.out.println("------------------");
+    //     for (int i = 0; i < allTshirtNames.size(); i++) {
+    //         System.out.println(allTshirtNames.get(i).getText() + " --- " + allTshirtPrices.get(i).getText());
+    //     }
+    // }
 
 }
